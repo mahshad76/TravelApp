67d9ab43c9c081ad0a96e0cdf305eba03dd60e6c
mahshad76 <mahshadmahdavimoghadam@gmail.com>
2026-03-07 16:03:04 -0500

    Dao is written

--- core/database/src/main/java/com/mahshad/database/UserDao.kt
+++ core/database/src/main/java/com/mahshad/database/UserDao.kt
@@ -1,4 +1,19 @@
 package com.mahshad.database
 
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+import kotlinx.coroutines.flow.Flow
+
+@Dao
 interface UserDao {
+    @Insert(onConflict = OnConflictStrategy.IGNORE)
+    suspend fun insertUser(user: UserEntity)
+
+    @Query("Delete FROM users_table WHERE user_email = :email")
+    suspend fun deleteUser(email: String)
+
+    @Query("SELECT * FROM users_table")
+    fun getUsers(): Flow<List<UserEntity>>
 }
--- core/database/src/main/java/com/mahshad/database/UserEntity.kt
+++ core/database/src/main/java/com/mahshad/database/UserEntity.kt
@@ -1,3 +1,21 @@
 package com.mahshad.database
 
-data class UserEntity()
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "users_table")
+data class UserEntity(
+    @PrimaryKey
+    @ColumnInfo("user_email")
+    val email: String,
+    @ColumnInfo("user_password")
+    val password: String
+) {
+    companion object {
+        val DEFAULT = UserEntity(
+            "mahshadmahdavi464@gmail.com",
+            password = "Helloworld77@"
+        )
+    }
+}
